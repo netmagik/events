6607d8a891170d5d32437a858bb92f837c953643
Irina <irina@netmagik.com>
2022-09-26 11:49:19 -0400

    Layout the index page in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        'bg-color': '#243c5a',
+        'bg-variant': '#2A9D8F',
+        'color-primary': '#E9C46A',
+        'color-primary-variant': '#F4A261',
+        'color-secondary': '#E76F51',
+        'color-primary-variant': '#F4A261',
+      },
+    },
   },
   plugins: [],
 }
